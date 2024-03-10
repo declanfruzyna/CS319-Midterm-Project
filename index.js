a4eb340eed07a43e929c90c28ce018fbb8bf1241
KainingGao <154624831+KainingGao@users.noreply.github.com>
2024-03-09 19:01:13 -0600

    delete edit and view buttons

--- index.js
+++ index.js
@@ -53,11 +53,7 @@ function movieSearchPropagator(movies) {
                 <a href="./moviePage.html#${movies[i].imdbID}"><img src=${moviePoster} class="card-img-top" alt="..."></img></a>
                 <div class="card-body">
                     <p class="card-text text"> <strong>${movies[i].Title}</strong>, ${movies[i].Year}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                        </div>
+                    <div class="d-flex justify-content-between align-items-center">                   
                         <small class="text-body-secondary">9 mins</small>
                     </div>
                 </div>
